0a5ff47ee88d7d38e991506156cd42fba7f6511e
forecho <caizhenghai@gmail.com>
2017-04-07 09:39:56 +0000

    优化代码

--- test.py
+++ test.py
@@ -4,6 +4,15 @@ from app import App
 import sys
 
 # python3 test.py https://movie.douban.com/review/8459684/
+# or
+# python3 test.py 8459684
+
 arg = sys.argv
-crawler = App(arg[1])
+key = arg[1]
+
+try:
+    url = "https://movie.douban.com/review/%d/" % int(key)
+except ValueError:
+    url = key
+crawler = App(url)
 print(crawler.send())
