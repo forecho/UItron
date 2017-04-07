bf2a542d9b3ea66ab101e55449ccd9ec2abcb940
forecho <caizhenghai@gmail.com>
2017-04-07 07:02:40 +0000

    add get

--- web.py
+++ web.py
@@ -5,6 +5,14 @@ from app import App
 
 
 class CrawlerResource(object):
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200  # This is the default status
+        resp.body = ('\nTwo things awe me most, the starry sky '
+                     'above me and the moral law within me.\n'
+                     '\n'
+                     '    ~ Immanuel Kant\n\n')
+
     def on_post(self, req, resp):
         """Handles POST requests"""
         req.auto_parse_form_urlencoded = True
